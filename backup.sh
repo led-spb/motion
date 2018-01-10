a3d382ba7e8038d79becfb006740433fe8f71bfa
Alexey.Ponimash <Alexey.Ponimash@gmail.com>
2018-01-10 15:19:54 +0300

    All configuration in one file

--- backup.sh
+++ backup.sh
@@ -2,23 +2,12 @@
 
 . $(dirname $0)/config
 
-###########
-FFMPEG=ffmpeg
-RETAIN_DAYS=14
-SOURCE=/home/hub/motion
-SRC_MASK="*_*.mp4"
-BACKUP_PATH="/media/yandex/motion"
-CODEC=h264_omx
-MTIME=1
-###########
-
-
 # Creating file for last day
 TMP_VIDEO=$(mktemp)
-TARGET_FILE=$SOURCE/$(date --date="-1 day" "+%Y-%m-%d.mp4")
+TARGET_FILE=$MEDIA_PATH/$(date --date="-1 day" "+%Y-%m-%d.mp4")
 
 #echo Checking data for last day
-find -H $SOURCE -daystart -type f -name "$SRC_MASK" -mtime $MTIME -print | sort |
+find -H $MEDIA_PATH -daystart -type f -name "$MEDIA_MASK" -mtime $MTIME -print | sort |
 while read filename; do
    echo Processing $(basename $filename)
    $FFMPEG -v quiet -i $filename -c:v copy -an -f mpegts -bsf h264_mp4toannexb pipe:1 >>$TMP_VIDEO
@@ -29,19 +18,19 @@ if [ -s "$TMP_VIDEO" ]
 then
   echo Encoding $TARGET_FILE
 
-  $FFMPEG -f mpegts -i $TMP_VIDEO -c:v $CODEC -an -y -f mp4 $TARGET_FILE && (
+  $FFMPEG -f mpegts -i $TMP_VIDEO -c:v $CODEC -b:v $BACKUP_BITRATE -an -y -f mp4 $TARGET_FILE && (
     echo Sending $TARGET_FILE
-    mosquitto_pub $MQTT_AUTH -t /home/camera/door/videom -f "$TARGET_FILE"
+    mosquitto_pub $MQTT -t $TOPIC_VIDEOM -f "$TARGET_FILE"
 
     echo Cleaning source files
-    find -H $SOURCE -daystart -type f -name "$SRC_MASK" -mtime $MTIME -delete
+    find -H $MEDIA_PATH -daystart -type f -name "$MEDIA_MASK" -mtime $MTIME -delete
   )
 else
   echo No data for last day
 fi
 
 echo Backup files
-find -H $SOURCE -type f -name "*.mp4" -mtime +1 | xargs -I {} mv {} $BACKUP_PATH/
+find -H $MEDIA_PATH -type f -name "*.mp4" -mtime +1 | xargs -I {} mv {} $BACKUP_PATH/
 
 # Remove jpeg files
 find -H $SOURCE -type f -name "*.jpg" -delete
--- event.sh
+++ event.sh
@@ -1,22 +1,22 @@
 #!/bin/sh
-
-. $(dirname $0)/config
+cd $(dirname $0)
+. ./config
 
 case "$1" in
    motion)
-     mosquitto_pub $MQTT_AUTH -t /home/sensor/motion_door -m 1 -r
+     mosquitto_pub $MQTT -t $TOPIC_MOTION -m 1 -r
      curl -s http://127.0.0.1:8082/0/action/snapshot >/dev/null 2>&1
      ;;
    motion_end)
-     mosquitto_pub $MQTT_AUTH -t /home/sensor/motion_door -m 0 -r
+     mosquitto_pub $MQTT -t $TOPIC_MOTION -m 0 -r
      ;;
    photo)
      # send photo to MQTT with retain option
-     mosquitto_pub $MQTT_AUTH -t /home/camera/door/photo -f "$2" -r
-     ln -f -s "$2" /home/hub/motion/lastsnap.jpg
+     mosquitto_pub $MQTT -t $TOPIC_PHOTO -f "$2" -r
+     ln -f -s "$2" "$MEDIA_PATH/lastsnap.jpg"
      ;;
    video)
-     /home/hub/motion/convert.sh "$2"
-     mosquitto_pub $MQTT_AUTH -t /home/camera/door/video -f "${2%%.*}.mp4"
+     ./convert.sh "$2"
+     mosquitto_pub $MQTT -t $TOPIC_VIDEO -f "$MEDIA_PATH/lastmotion.mp4"
      ;;
 esac
